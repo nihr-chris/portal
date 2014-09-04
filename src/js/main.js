0be765f4aed2a97464702d62fe9528e888f4627f
Chris Devereux <devereux.chris@gmail.com>
2014-09-04 12:14:01 +0000

    Add navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 var Ractive = window.Ractive;
 var template = require('template');
 
+console.log(template("sidebar"));
+
 var r = new Ractive({
     el: 'sidebar',
-    template: template("sidebar")
+    template: template("sidebar"),
+    data: {
+        navbarOptions: [
+            {title: "Overview", active: false},
+            {title: "Recruitment", active: true},
+            {title: "Time & Target", active: false},
+            {title: "Engagement", active: false}
+        ]
+    }
 });
